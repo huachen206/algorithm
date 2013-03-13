9bb13b0acdd4050bad29e36b60afb812eeeb27ed
花 晨 <hanaashita@hanatekiMac.local>
2013-03-13 20:06:23 +0800

    Initial Commit

---
+++ Algorithm/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Algorithm
+//
+//  Created by 花 晨 on 13-3-13.
+//  Copyright (c) 2013年 花 晨. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
